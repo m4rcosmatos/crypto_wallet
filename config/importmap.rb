646981c46fceddecacaf5cb34145e2da777190c2
m4rcosmatos <102394275+m4rcosmatos@users.noreply.github.com>
2022-09-13 14:14:22 -0300

    Update importmap.rb

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,5 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.1/dist/jquery.js"
-pin "jquery", to: "jquery.js", preload: true
-pin "jquery_ujs", to: "jquery_ujs.js", preload: true
-pin "notify", to: "notify.js", preload: true
+pin "jquery", to: "https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.js"
+pin "notify", to: "https://rawgit.com/notifyjs/notifyjs/master/dist/notify.js"
