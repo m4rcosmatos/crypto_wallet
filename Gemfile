9fb13684f9b35d31b9b5988cc0bde4caecc373ca
m4rcosmatos <102394275+m4rcosmatos@users.noreply.github.com>
2022-09-22 22:25:45 -0300

    utilizando o yarn

--- Gemfile
+++ Gemfile
@@ -74,6 +74,4 @@ group :test do
   gem "webdrivers"
 end
 
-gem 'jquery-rails'
 
-gem 'notifyjs_rails'
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 //= require jquery
-//= require notifyjs_rails
+//= require notify-js-legacy/notify
+//= require bootstrap/dist/js/bootstrap
+//= require popper.js/dist/popper
